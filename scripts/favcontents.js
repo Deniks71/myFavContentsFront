994e0fd7ea0edfe5a74df71ce00626a9df030a24
Denilson Nascimento <denilsonn7187@gmail.com>
2024-10-06 23:05:52 -0300

    Finalizando a parte de delete de elementos na pagina

--- scripts/favcontents.js
+++ scripts/favcontents.js
@@ -4,6 +4,18 @@
 // })
 getContents();
 const addButtonElement = document.querySelector('#insereConteudo_button');
+const buttonDeleteElement = document.querySelectorAll('.delete-btn');
+const contentContainer = document.querySelector('.content_container');
+
+contentContainer.addEventListener('click', (event) => {
+   console.log(event)
+   if(event.target.classList.contains('delete-btn')){
+        const clickedButton = event.target;
+        const idFromDataAttribute = clickedButton.dataset.id
+        deleteContent(idFromDataAttribute)
+   }
+})
+
 
 addButtonElement.addEventListener('click', () => {
     const titleElement = document.querySelector('#inputconteudo');
@@ -24,7 +36,7 @@ addButtonElement.addEventListener('click', () => {
     descriptionElement.value = ""
 })
 
-
+ 
 async function getContents () {
     
     
@@ -42,6 +54,9 @@ async function getContents () {
         });
 
         const jsonResponse = await response.json();
+
+        const contentContainer = document.querySelector('.content_container');
+        contentContainer.innerHTML = ''; // Limpa o conteúdo existente
 
         jsonResponse.conteudos.forEach(content => {
             const contentContainer = document.querySelector('.content_container');
@@ -114,3 +129,26 @@ async function addContent (title,description) {
 
 };
 
+async function deleteContent (idContent) {
+    const user = JSON.parse(localStorage.getItem('user'));
+    console.log(user);
+
+    try{
+        const response = await fetch(`http://localhost:3000/myfavcontent/user/${user.id}/delete/${idContent}`, {
+            method: 'DELETE',
+            headers: {
+                'Authorization': `Bearer ${user.token}`,
+                'Content-Type': 'application/json'
+            }
+            
+        })
+
+        getContents();
+        console.log(response);
+        
+        
+    } catch (err){
+        console.error('Erro:', err);
+    }
+    
+}
